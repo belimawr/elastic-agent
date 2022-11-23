c9024b24c9a867ba42bce93853b3fd7bf41dc4e7
Tiago Queiroz <tiago.queiroz@elastic.co>
2022-11-23 16:57:36 +0100

    Test coordinator.Upgrade
    Update the unittests to test the 'new upgrade path' under V2.

--- internal/pkg/agent/application/upgrade/upgrade_integration_test.go
+++ internal/pkg/agent/application/upgrade/upgrade_integration_test.go
@@ -4,19 +4,21 @@ package upgrade
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 
 	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/coordinator"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/info"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/reexec"
-	"github.com/elastic/elastic-agent/internal/pkg/artifact"
+	"github.com/elastic/elastic-agent/internal/pkg/agent/application/upgrade/artifact"
 	"github.com/elastic/elastic-agent/internal/pkg/fleetapi"
-	"github.com/elastic/elastic-agent/internal/pkg/reporter"
-	"github.com/elastic/elastic-agent/internal/pkg/reporter/noop"
-	//	_ "github.com/elastic/elastic-agent/internal/pkg/fleetapi/acker/noop" //acker "github.com/elastic/elastic-agent/internal/pkg/fleetapi/acker/noop"
+	"github.com/elastic/elastic-agent/pkg/component"
 )
 
 type mockAction struct {
@@ -46,14 +48,18 @@ type nopAcker struct{}
 func (n nopAcker) Ack(ctx context.Context, action fleetapi.Action) error { return nil }
 func (n nopAcker) Commit(ctx context.Context) error                      { return nil }
 
-const mockAgentScript = `#!/bin/sh
-echo foo
+const mockAgentScriptTpl = `#!/bin/sh
+echo "reexec-called" >> %s
 exit 0
 `
 
 // SetupPathsForTesting creates all folders/files necessary for the integration
 // tests on this file. It also sets some global values using the `paths` package.
-func setupPathsForTesing(t *testing.T) {
+//
+// The returned values are:
+// - The temporary directory
+// - The reexec log file.
+func setupPathsForTesing(t *testing.T) (string, string) {
 	t.Helper()
 	targetDirectory := t.TempDir()
 
@@ -72,7 +78,8 @@ func setupPathsForTesing(t *testing.T) {
 		t.Fatalf("cannot create 'elastic-agent binary' file: %s", err)
 	}
 
-	//	fmt.Fprint(agentFile, mockAgentScript)
+	reexecLogFile := filepath.Join(targetDirectory, "reexec-called")
+	mockAgentScript := fmt.Sprintf(mockAgentScriptTpl, reexecLogFile)
 	if _, err := agentFile.WriteString(mockAgentScript); err != nil {
 		t.Fatal(err)
 	}
@@ -81,37 +88,50 @@ func setupPathsForTesing(t *testing.T) {
 	}
 
 	t.Logf("Paths: Download: %s, Install: %s, Home: %s", paths.Downloads(), paths.Install(), paths.Home())
+
+	return targetDirectory, reexecLogFile
+}
+
+// setLogLevel configures logp in development mode if -test.v is true
+//
+// This helps a lot to debug failing tests
+func setLogLevel(t *testing.T) {
+	t.Helper()
+	if !flag.Parsed() {
+		t.Fatal("not parsed")
+	}
+
+	f := flag.CommandLine.Lookup("test.v")
+	if f == nil {
+		t.Fatal("test flag not set")
+	}
+
+	strVal := f.Value.String()
+	verbose, err := strconv.ParseBool(strVal)
+	if err != nil {
+		// This should never happen because the testing package already ensures
+		// that test.v is a valid boolean.
+		t.Logf("cannot parse test.v='%s' as boolean, disabling logging.", strVal)
+	}
+
+	if verbose {
+		logp.DevelopmentSetup()
+		t.Log("logp level set as DevelopmentSetup")
+	}
 }
 
 // TestUpgrade unit-tests upgrader.TestUpgrade
-// go test -v -tags=integration -run=Upgrade -ldflags="-X github.com/elastic/elastic-agent/internal/pkg/release.allowUpgrade=true -X github.com/elastic/elastic-agent/version.commit=188688"
-// dlv test --build-flags="-ldflags='-X github.com/elastic/elastic-agent/internal/pkg/release.allowUpgrade=true -X github.com/elastic/elastic-agent/version.commit=188688' -tags=integration" . -- -test.run=Upgrade
+// go test -v -tags=integration -run=Upgrade -ldflags="-X github.com/elastic/elastic-agent/internal/pkg/release.allowEmptyPgp=true -X github.com/elastic/elastic-agent/internal/pkg/release.allowUpgrade=true"
+// dlv test --build-flags="-ldflags='-X github.com/elastic/elastic-agent/internal/pkg/release.allowEmptyPgp=true -X github.com/elastic/elastic-agent/internal/pkg/release.allowUpgrade=true' -tags=integration" . -- -test.run=Upgrade
 func TestUpgrade(t *testing.T) {
-	logp.DevelopmentSetup()
+	setLogLevel(t)
 	setupPathsForTesing(t)
-
-	// if err := os.Mkdir(filepath.Join(os.Getwd(), "elastic-agent-"+version.Commit()), 0750); err != nil {
-	// 	t.Fatalf("cannot create dir: %s", err)
-	// }
 
 	agentInfo := &info.AgentInfo{}
 
-	reporter := reporter.NewReporter(context.TODO(), logp.L(), agentInfo, noop.NewReporter())
-
-	upgrader := NewUpgrader(
-		agentInfo,
-		artifact.DefaultConfig(),
-		logp.L(), // I hate global state!
-		[]context.CancelFunc{},
-		nopReExec{}, //reexecManager
-		nopAcker{},  //acker
-		reporter,    //stateReporter
-		nil,         //capabilities.Capability
-	)
-
-	callback, err := upgrader.Upgrade(context.Background(), mockAction{version: "8.4.0"}, false)
+	upgrader := NewUpgrader(logp.L(), artifact.DefaultConfig(), agentInfo)
+	callback, err := upgrader.Upgrade(context.Background(), "8.7.0", "file:///home/tiago/sandbox/dropPath", nil)
 	if err != nil {
-		os.Exit(1)
 		t.Fatalf("could not execute upgrade: %s", err)
 	}
 
@@ -119,5 +139,28 @@ func TestUpgrade(t *testing.T) {
 		if err := callback(); err != nil {
 			t.Fatalf("error calling callback: %s", err)
 		}
+	}
+}
+
+// TestCoordinator unit-tests cordinator.Upgrade
+// go test -v -tags=integration -run=Coordinator -ldflags="-X github.com/elastic/elastic-agent/internal/pkg/release.allowEmptyPgp=true -X github.com/elastic/elastic-agent/internal/pkg/release.allowUpgrade=true"
+// dlv test --build-flags="-ldflags='-X github.com/elastic/elastic-agent/internal/pkg/release.allowEmptyPgp=true -X github.com/elastic/elastic-agent/internal/pkg/release.allowUpgrade=true' -tags=integration" . -- -test.run=Coordinator
+func TestCoordinator(t *testing.T) {
+	setLogLevel(t)
+	_, reexecLogFile := setupPathsForTesing(t)
+
+	agentInfo := &info.AgentInfo{}
+
+	upgrader := NewUpgrader(logp.L(), artifact.DefaultConfig(), agentInfo)
+	coord := coordinator.New(logp.L(), agentInfo, component.RuntimeSpecs{}, nopReExec{}, upgrader, nil, nil, nil, nil, nil)
+
+	err := coord.Upgrade(context.Background(), "8.7.0", "file:///home/tiago/sandbox/dropPath", nil)
+	if err != nil {
+		t.Fatalf("could not execute upgrade: %s", err)
+	}
+
+	if _, err := os.Stat(reexecLogFile); err != nil {
+		t.Fatalf("re-exec log file does not exisit, this usually means the"+
+			" reexec operation/callback did not run successfully: %s", err.Error())
 	}
 }
